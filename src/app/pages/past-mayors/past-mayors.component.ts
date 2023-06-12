4955acb9943653be671eb91b4b64f15e7aa9b253
Andres Forero <anfforerove@gmial.com>
2023-06-12 02:15:45 -0500

    Limit exceeded firestore, make a smaller bd

--- src/app/pages/past-mayors/past-mayors.component.ts
+++ src/app/pages/past-mayors/past-mayors.component.ts
@@ -1,41 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PastMayors } from 'src/app/models/past-mayor';
 import { PastMayorsService } from 'src/app/services/mayors/past-mayors.service';
+import { csv } from 'd3';
 
 @Component({
   selector: 'app-past-mayors',
   templateUrl: './past-mayors.component.html',
   styleUrls: ['./past-mayors.component.scss'],
 })
-export class PastMayorsComponent {
+export class PastMayorsComponent implements OnInit {
   cardData?: PastMayors[];
+  collection: string = '2007';
+  dataUp?: PastMayors[];
 
   constructor(private _pastMayorService: PastMayorsService) {}
 
-  async pruebaCarga() {
-    const result = await this._pastMayorService.addPastMayor(this.dataOnePastMayor);
-    console.log(result)
+  ngOnInit(): void {
+    this.convertirDatos();
   }
 
-  dataOnePastMayor: PastMayors = {
-    ano: 2019,
-    censoe_hombres: 1234,
-    censoe_mujeres: 1234,
-    censoe_total: 2345,
-    circunscripcion: 'Municipal',
-    coddpto: 2345,
-    codigo_lista: 2345,
-    codigo_partido: 1234,
-    codmpio: 2345,
-    curules: 2345,
-    departamento: 'ANTIOQUIA',
-    fecha_eleccion: 'Octubre 27',
-    id_electoral: 2345,
-    municipio: 'SAN JUAN DE URABA',
-    nombres: 'LUIS FERNANDO',
-    primer_apellido: 'RUIZ',
-    segundo_apellido: 'DIAZ',
-    tipo_eleccion: 'Alcaldía Municipal',
-    votos: 2345,
-  };
+  async pruebaCarga() {
+    console.log("enviando");
+    if (this.dataUp === undefined) {
+      console.log("Error al traer los archivos '.csv'");
+      return;
+    }
+    this.dataUp.forEach(async (mayor) => {
+      try {
+        await this._pastMayorService.addPastMayor(mayor, this.collection);
+      } catch (error) {
+        console.log('Error al crear el documento');
+      }
+    });
+  }
+
+  async convertirDatos() {
+    try {
+      const data: any[] = await csv(
+        `../../../assets/data/past-mayors-data/${this.collection}_alcaldia.dta.csv`
+      );
+      this.dataUp = data;
+      console.log(this.dataUp);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
